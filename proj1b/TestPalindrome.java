1b50f94192b3864fef9d1e7b2ca471ab07105ae1
kkkunnnnethan <dickson981127@gmail.com>
2020-04-05 19:06:47 +0800

    Finder commit

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -1,9 +1,11 @@
-/*import org.junit.Test;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
+
+    //不懂為什麼要特別宣告一個Constructor
     static Palindrome palindrome = new Palindrome();
 
     @Test
@@ -15,4 +17,36 @@ public class TestPalindrome {
         }
         assertEquals("persiflage", actual);
     }
-}     Uncomment this class once you've created your Palindrome class. */
+
+    @Test
+    public void testIsPalindrome(){
+        assertFalse(palindrome.isPalindrome("cat"));
+        assertTrue(palindrome.isPalindrome("racecar"));
+        assertFalse(palindrome.isPalindrome("Racecar"));
+        assertTrue(palindrome.isPalindrome("a"));
+        assertTrue(palindrome.isPalindrome(""));
+    }
+
+    @Test
+    public void testIsPalindromeExtra(){
+        assertFalse(palindrome.isPalindrome("121"));
+    }
+
+    @Test
+    public void testIsPalindromeOverload(){
+        CharacterComparator offByOne = new OffByOne();
+        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+        assertTrue(palindrome.isPalindrome("flake", offByOne));
+        assertTrue(palindrome.isPalindrome("zyzy", offByOne));
+        assertTrue(palindrome.isPalindrome("yyxz", offByOne));
+        assertTrue(palindrome.isPalindrome("yyyxz", offByOne));
+        assertFalse(palindrome.isPalindrome("aa", offByOne));
+    }
+
+    @Test
+    public void testIsPalindromeOverloadExtra(){
+        CharacterComparator offByOne = new OffByOne();
+        assertFalse(palindrome.isPalindrome("121", offByOne));
+    }
+}
